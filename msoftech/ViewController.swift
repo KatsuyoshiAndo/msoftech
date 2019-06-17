1934a9ca64e8cfcb8c7fbe84d198d99ae93165ea
KatsuyoshiAndo <dev.katsuyoshiando@gmail.com>
2019-06-17 16:04:44 +0900

    Initial Commit

---
+++ msoftech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  msoftech
+//
+//  Created by matakino on 2019/06/17.
+//  Copyright © 2019 KatsuyoshiAndo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
